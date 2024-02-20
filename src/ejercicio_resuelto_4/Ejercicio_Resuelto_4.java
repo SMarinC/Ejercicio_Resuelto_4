7f260cdc455f5808abd8035f7c09d41c9dba8f64
Santiago Marin Cardona <marincardona.santiago@hotmail.com>
2024-02-20 17:26:02 -0500

    Initial commit

---
+++ src/ejercicio_resuelto_4/Ejercicio_Resuelto_4.java
@@ -0,0 +1,19 @@
+
+package ejercicio_resuelto_4;
+
+public class Ejercicio_Resuelto_4 {
+
+    public static void main(String[] args) {
+        double EdJuan,  EdAlber, EdAna, EdMama;
+        EdJuan = 9;
+        EdAlber = (EdJuan * 2) / 3;
+        EdAna = (EdJuan * 4) / 3;
+        EdMama = EdJuan + EdAlber + EdAna;
+        System.out.println("Las edades son: ");
+        System.out.println("Alberto: " + EdAlber);
+        System.out.println("Juan: " + EdJuan);
+        System.out.println("Ana: " + EdAna);
+        System.out.println("Mama: " + EdMama);
+    }
+    
+}
